b63ec1d6bfdb66196d8566a6825a7ac3c51a9d73
XR <xr@192.168.10.128>
2018-04-26 10:54:22 -0700

    commit - 20180426 	modified:   Snoring Companion.xcodeproj/project.pbxproj 	modified:   Snoring Companion/AppDelegate.swift 	modified:   Snoring Companion/Base.lproj/Main.storyboard 	modified:   Snoring Companion/Controller/HomeViewController.swift 	modified:   Snoring Companion/Info.plist 	modified:   Snoring Companion/Model/Global/Global.swift

--- Snoring Companion/Controller/HomeViewController.swift
+++ Snoring Companion/Controller/HomeViewController.swift
@@ -14,11 +14,26 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.setupView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func setupView() {
+        
+        self.view.backgroundColor = Global.APP_MAIN_BACKGROUND
+        self.navigationController?.isNavigationBarHidden = true
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [Global.APP_MAIN_BACKGROUND.cgColor, Global.GRADIENT_COLOR1.cgColor, Global.GRADIENT_COLOR2.cgColor, Global.GRADIENT_COLOR1.cgColor]
+        gradientLayer.locations = [0.0, 0.2, 0.45, 0.9]
+//        self.view.layer.addSublayer(gradientLayer)
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
 
--- Snoring Companion/Model/Global/Global.swift
+++ Snoring Companion/Model/Global/Global.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+
+
 class Global: NSObject {
 
+    static let APP_MAIN_BACKGROUND = UIColor(red: CGFloat(0.0 / 255.0), green: CGFloat(21.0 / 255.0), blue: CGFloat(50.0 / 255.0), alpha: 1.0)
+    static let GRADIENT_COLOR1 = UIColor(red: CGFloat(15.0 / 255.0), green: CGFloat(103.0 / 255.0), blue: CGFloat(150.0 / 255.0), alpha: 1.0)
+    static let GRADIENT_COLOR2 = UIColor(red: CGFloat(70.0 / 255.0), green: CGFloat(149.0 / 255.0), blue: CGFloat(188.0 / 255.0), alpha: 1.0)
 }
